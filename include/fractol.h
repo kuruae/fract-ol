b11721a05f0937f956caf8af1d724d5c608e78cd
kuruae <kurutsukipv@gmail.com>
2024-09-13 15:08:47 +0200

    -

--- include/fractol.h
+++ include/fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: emagnani <emagnani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 14:44:37 by enzo              #+#    #+#             */
-/*   Updated: 2024/09/11 10:31:11 by enzo             ###   ########.fr       */
+/*   Updated: 2024/09/13 14:53:19 by emagnani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,53 @@ typedef struct s_fractal
 {
 	void	*mlx;
 	void	*window;
-}	t_fractal;
+	void	*image;
+	void	*img_ptr;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian; // ???
+	int		color;
+	int		max_iterations;
+	int		x;
+	int		y;
+	double	zx;
+	double	zy;
+	double	cx;
+	double	cy;
+	double	zoom;
+	char	*name;
+}			t_fractal;
+
+# ifdef __APPLE__
+#  define SCROLL_UP 4
+#  define SCROLL_DOWN 5
+#  define UP 126
+#  define LEFT 123
+#  define RIGHT 124
+#  define DOWN 125
+#  define ESC 53
+#  define R 15
+#  define Q 12
+#  define W 13
+# else
+#  define SCROLL_UP 4
+#  define SCROLL_DOWN 5
+#  define UP 65362
+#  define LEFT 65361
+#  define RIGHT 65363
+#  define DOWN 65364
+#  define ESC 65307
+#  define R 27
+#  define Q 113
+#  define W 119
+# endif
+
 
 #endif
+
+/*
+c = complex (imaginary numbers)
+z = integers set (real numbers)
+x = real axis
+y = imaginary axis
+*/
